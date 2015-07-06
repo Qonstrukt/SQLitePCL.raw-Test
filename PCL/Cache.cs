3fdce4d8d61fd3f2b57d8593afcd28e75dc80a06
Qonstrukt <stefan.moonen@freshheads.com>
2015-07-06 18:07:36 +0200

    Initial commit

---
+++ PCL/Cache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Akavache;
+
+namespace PCL
+{
+	public static class Cache
+	{
+		public static IBlobCache GetCache ()
+		{
+			BlobCache.ApplicationName = "Test";
+
+			BlobCache.LocalMachine.InsertObject ("test", "dummy");
+
+			return BlobCache.LocalMachine;
+		}
+	}
+}
+
